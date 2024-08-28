3427a36897d796efcb2447aa14a6f3963ae7b851
amaliebernes <bernesamalie@gmail.com>
2024-08-28 12:03:19 +0200

    Script

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log('Hello!');
+let greeting = "Hello kodeloftet"
+
+console.log(greeting);
+
+
+// dette er en kommentar
+
+/**
+ * kommentar 
+ * over
+ * flere 
+ * linjer
+ */
+let a = 10
+let b = 32
+let c = a + b
+
+console.log(c)
+
+
+let lars = {
+    name: "Amalie Bernes",
+    age: 35,
+    alive: true,
+}
+
+console.log(lars)
+
+console.log(lars.name)
+
+
+if
+
+    (lars.alive) {
+    console.log("Alive!")
+
+}
+else {
+    console.log("Dead!")
+
+}
